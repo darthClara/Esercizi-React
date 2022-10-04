f0afe9444ae63923652389b57741f275e464fb85
Clara Schillaci <dhdjjk>
2022-10-04 15:08:33 +0200

    fixed master branch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import Esercizio from "./components/Esercizio";
 
 function App() {
     return (
-        <Esercizio />
+
   );
 }
 
--- src/components/Esercizio.js
+++
@@ -1,9 +0,0 @@
-const hello = <h1>Hello world!</h1>;
-
-function Esercizio() {
-    return (
-        hello
-    );
-}
-
-export default Esercizio;
